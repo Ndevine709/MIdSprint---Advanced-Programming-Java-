c8aa8c71f2919f7faf1d7241d28bb715f3946a98
Laura Wiseman <laurawiseman_90@hotmail.com>
2025-02-17 13:35:23 -0330

    Adjusted code for Prescription.java
    - Adjusted some errors in the getters and setters.

--- Prescription.java
+++ Prescription.java
@@ -36,7 +36,7 @@ public Doctor getDoctor(){
     return doctor;
 }
 
-public Doctor setDoctor(){
+public void setDoctor(Doctor doctor){
     this.doctor = doctor;
 }
 
@@ -44,7 +44,7 @@ public Patient getPatient(){
     return patient;
 }
 
-public Patient setPatient(Patient patient){
+public void setPatient(Patient patient){
     this.patient = patient;
 }
 
@@ -60,7 +60,7 @@ public LocalDate getDateIssued(){
     return dateIssued;
 }
 
-public LocalDate setDateIssued(LocalDate dateIssued){
+public void setDateIssued(LocalDate dateIssued){
     this.dateIssued = dateIssued;
 }
 
